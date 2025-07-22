9ba21ba4657ab8143019a27d9ad357c9a3d8b29e
Nazaret <nazaretgutirrezmartn@gmail.com>
2025-07-22 16:35:38 +0200

    Iteration 1

--- src/movies.js
+++ src/movies.js
@@ -1,30 +1,36 @@
 // Iteration 1: All directors? - Get the array of all directors.
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors.
 // How could you "clean" a bit this array and make it unified (without duplicates)?
-function getAllDirectors(moviesArray) {}
+function getAllDirectors(movies) {
+  const directors = movies.map((movie) => movie.director);
+  const uniqueDirectors = directors.filter((director, index) => {
+    return directors.indexOf(director) === index;
+  });
 
-// Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
-function howManyMovies(moviesArray) {}
+  return uniqueDirectors;
+}
+getAllDirectors(movies);
 
-// Iteration 3: All scores average - Get the average of all scores with 2 decimals
-function scoresAverage(moviesArray) {}
+// // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
+// function howManyMovies(moviesArray) {}
 
-// Iteration 4: Drama movies - Get the average of Drama Movies
-function dramaMoviesScore(moviesArray) {}
+// // Iteration 3: All scores average - Get the average of all scores with 2 decimals
+// function scoresAverage(moviesArray) {}
 
-// Iteration 5: Ordering by year - Order by year, ascending (in growing order)
-function orderByYear(moviesArray) {}
+// // Iteration 4: Drama movies - Get the average of Drama Movies
+// function dramaMoviesScore(moviesArray) {}
 
-// Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
-function orderAlphabetically(moviesArray) {}
+// // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
+// function orderByYear(moviesArray) {}
 
-// BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
-function turnHoursToMinutes(moviesArray) {}
+// // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
+// function orderAlphabetically(moviesArray) {}
 
-// BONUS - Iteration 8: Best yearly score average - Best yearly score average
-function bestYearAvg(moviesArray) {}
+// // BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
+// function turnHoursToMinutes(moviesArray) {}
 
-
+// // BONUS - Iteration 8: Best yearly score average - Best yearly score average
+// function bestYearAvg(moviesArray) {}
 
 // The following is required to make unit tests work.
 /* Environment setup. Do not modify the below code. */
@@ -37,6 +43,6 @@ if (typeof module !== 'undefined') {
     orderByYear,
     orderAlphabetically,
     turnHoursToMinutes,
-    bestYearAvg,
+    bestYearAvg
   };
 }
